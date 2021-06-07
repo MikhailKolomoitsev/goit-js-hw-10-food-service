b72128f38e10fe5b704ec3469f60e5ce800c3bdf
mihon.testosterone <mihon.testosterone@gmail.com>
2021-06-07 20:00:33 +0300

    2
    2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-import './sass/main.scss';
+import cards from './menu.json';
+import itmesTemplate from './templates/food-cards.hbs'
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+const menuRef = document.querySelector('.js-menu')
+const markUp = itmesTemplate(cards)
+
+menuRef.insertAdjacentHTML('beforeend', markUp)
